d74870eccbb7a2a8e8ad9a6daf4183034073d403
Oleh Liubimov <mtbs1996@gmail.com>
2024-04-20 17:46:34 +0300

    1st task done

---
+++ react-hw-1/src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import Profile from './components/profile/Profile'
+import userData from "./userData.json"
+
+function App() {
+  
+
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  )
+}
+
+export default App
---
+++ react-hw-1/src/components/profile/Profile.jsx
@@ -0,0 +1,31 @@
+import css from "./Profile.module.css"
+
+
+
+function Profile(userData) {
+    return (
+    <div className={css.wrapper}>
+        <div className={css.userData}>
+            <img className={css.userImg} src={userData.image} alt="User avatar" />
+           <p className={css.userName}>{userData.name}</p>
+           <p className={css.userTag}>@{userData.tag}</p>
+           <p className={css.userLocation}>{ userData.location}</p>
+        </div>
+        <ul className={css.profileList}>
+            <li className={css.profileListItem}>
+                <span className={css.statsText}>Folowers</span>
+               <span className={css.statsNums}>{userData.stats.followers}</span>
+            </li>
+            <li className={css.profileListItem}>
+                <span className={css.statsText}>Views</span>
+               <span className={css.statsNums}>{userData.stats.views}</span>
+            </li>
+            <li className={css.profileListItem}>
+                <span className={css.statsText}>Likes</span>
+               <span className={css.statsNums}>{ userData.stats.likes}</span>
+            </li>
+        </ul>
+    </div>)
+}
+
+export default Profile
